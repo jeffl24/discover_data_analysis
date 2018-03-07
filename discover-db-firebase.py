be5a21a6c4fc989b9d5a9787bd0dcac864b071a0
Jeff <limziwangis@gmail.com>
2018-03-07 15:27:52 +0800

    completed code for pulling from database and gitignore

--- discover-db-firebase.py
+++ discover-db-firebase.py
@@ -6,12 +6,41 @@ from firebase_admin import credentials
 from firebase_admin import db
 pd.options.display.max_columns = 999
 
-cred = credentials.Certificate('D:\Code\discover_data_analysis\discover-db-firebase.json')
+cred = credentials.Certificate('D:\Code\discover-db-firebase.json')
 firebase_admin.initialize_app(cred, {'databaseURL': 'https://discover-db.firebaseio.com/'})
 
-snapshot_custom = db.reference('results').order_by_child('timestamp').limit_to_last(10).get()
+snapshot_custom = db.reference('results').order_by_key().start_at('-KtdoxwhQLhxA19-YAEJ').get()
 
 
 # .start_at('b').end_at(u'b\uf8ff')
+key_series = []
 for key in snapshot_custom:
-    print(key)
+    key_series.append(key)
+
+# print(key_series)
+# This loops through the key_series list generated earlier and
+# for each key, make a query and format the data
+snapshot_drop_list = ['Barcode',
+             'first_description',
+             'image_url',
+             'ingredients',
+             'key_benefits',
+             'key_ingredients',
+             'research',
+             'shopifyID',
+             'url',
+             'usa_url' ]
+
+discover_df_all = pd.DataFrame()
+for entry in key_series:
+    each_snapshot = dict(db.reference('results').order_by_key().start_at(entry).limit_to_first(1).get())[entry]
+    if 'email' not in each_snapshot['request']:
+        continue
+    left_df = pd.io.json.json_normalize(each_snapshot)
+    right_df = pd.io.json.json_normalize(each_snapshot, record_path='results').drop(snapshot_drop_list, axis=1)
+    right_df['email'] = each_snapshot['request']['email']
+    new_df = pd.merge(left_df, right_df, left_on='request.email', right_on='email').drop(['request.email', 'results'], axis=1)
+    discover_df_all = discover_df_all.append(new_df)
+
+discover_df_all.to_csv('discover_print_to_csv.csv')
+print('Printed to CSV')
