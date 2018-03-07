3755ec3a2f2b50eada9755b32dd30151e076862a
Jeff <limziwangis@gmail.com>
2018-03-07 12:29:50 +0800

    removed some files

--- discover-db-firebase.py
+++ discover-db-firebase.py
@@ -4,10 +4,14 @@ import json
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
+pd.options.display.max_columns = 999
 
 cred = credentials.Certificate('D:\Code\discover_data_analysis\discover-db-firebase.json')
-firebase_admin.initialize_app(cred,
-                              {'databaseURL': 'https://discover-db.firebaseio.com/'})
-ref = db.reference('results/-L-F3vs_iWKf3O6yXN_P')
+firebase_admin.initialize_app(cred, {'databaseURL': 'https://discover-db.firebaseio.com/'})
 
-print(ref.order_by_key().limit_to_last(1).get())
+snapshot_custom = db.reference('results').order_by_child('timestamp').limit_to_last(10).get()
+
+
+# .start_at('b').end_at(u'b\uf8ff')
+for key in snapshot_custom:
+    print(key)
